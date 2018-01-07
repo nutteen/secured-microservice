591b08d4a82b52fc1f2fa904f2ebf2d4cf46795d
Peeranut <peeranut.ng@grouplease.co.th>
2018-01-07 21:28:52 +0700

    [PurchaseService] Configured Keycloak for Purchase Service with Zuul Aware filter

---
+++ purchase-service/src/main/java/com/example/securedmicroservice/purchaseservice/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.example.securedmicroservice.purchaseservice;
+
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+/**
+ * Copyright (c) Group Lease Public Company Limited. All rights reserved. (http://www.grouplease.co.th/)
+ * Author: Peeranut Ngaorungsri (peeranut.ng@grouplease.co.th) on 06/01/2018.
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
+    http.authorizeRequests().antMatchers("/sso/login", "/auth/").permitAll().antMatchers("/**").authenticated();
+    http.csrf().disable();
+  }
+
+  /**
+   * Registers the KeycloakAuthenticationProvider with the authentication
+   * manager.
+   */
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(keycloakAuthenticationProvider());
+  }
+
+  @Override
+  protected AuthenticationEntryPoint authenticationEntryPoint() {
+    return new ZuulAwareKeycloakAuthenticationEntryPoint();
+  }
+
+  @Bean
+  public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+          KeycloakAuthenticationProcessingFilter filter) {
+    FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+    registrationBean.setEnabled(false);
+    return registrationBean;
+  }
+
+  @Bean
+  public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(KeycloakPreAuthActionsFilter filter) {
+    FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+    registrationBean.setEnabled(false);
+    return registrationBean;
+  }
+
+  @Bean
+  @Override
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
+}
